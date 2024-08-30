1d21f8d94643717de97bc118a97132b1cca4a6cb
Akshay Nair <akshayakn6@gmail.com>
2024-08-29 22:16:23 -0400

    feat: Lightbox for gallery

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,14 +1,32 @@
-import { Title, SimpleGrid, Container, Skeleton, useMantineTheme, Box } from "@mantine/core";
+import {
+  Title,
+  SimpleGrid,
+  Container,
+  Skeleton,
+  useMantineTheme,
+  Box,
+} from "@mantine/core";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { CollectionName } from "../components/data/constants";
 import { db } from "../components/data/firebaseConfig";
 import Head from "next/head";
-
+import { Gallery as GridGallery } from "react-grid-gallery";
+import Lightbox, { SlideImage } from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { Image } from "react-grid-gallery";
 const getChild = (image: string, height: number) => (
-  <Box style={{ background: "url(" + image + ")", height: height, borderRadius: 5 }} />
+  <Box
+    style={{
+      background: "url(" + image + ")",
+      height: height,
+      borderRadius: 5,
+    }}
+  />
 );
-const getChildSkeleton = (height: number) => <Skeleton style={{ height: height, borderRadius: 5 }} />;
+const getChildSkeleton = (height: number) => (
+  <Skeleton style={{ height: height, borderRadius: 5 }} />
+);
 const BASE_HEIGHT = 360;
 const getSubHeight = (children: number, spacing: number) =>
   BASE_HEIGHT / children - spacing * ((children - 1) / children);
@@ -34,6 +52,9 @@ const images = [
 ];
 export default function Gallery() {
   const [images, setImages] = useState<MediaProps[]>();
+  const [galleryImages, setGalleryImages] = useState<Image[]>([]);
+  const [slideImages, setSlideImages] = useState<SlideImage[]>([]);
+  const [index, setIndex] = useState(-1);
   const theme = useMantineTheme();
   useEffect(() => {
     const run = async () => {
@@ -41,25 +62,61 @@ export default function Gallery() {
       const docSnap = await getDoc(docRef);
       console.log({ ...docSnap.data() });
       setImages(docSnap.data()?.images);
+      console.log(images);
     };
     run();
   }, []);
+  useEffect(()=>{
+    let gimg = images?.map((item)=>{
+      return {
+        src: item.url,
+        original: item.url,
+        width: 0,
+        height: 0,
+      }
+    })
+    setGalleryImages(gimg as Image[])
+    let simg = images?.map(item=>{
+      return {
+        src: item.url,
+        original: item.url,
+      }
+    })
+    setSlideImages(simg as SlideImage[])
+  },[images])
+  const handleClick = (index: number, item: Image) => setIndex(index);
+
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>
-          JMount Gallery - A Picture is worth a Thousand Words, See the Beauty of Vagamon through our Gallery
+          JMount Gallery - A Picture is worth a Thousand Words, See the Beauty
+          of Vagamon through our Gallery
         </title>
         <meta
           name="description"
           content="Explore the idyllic views of Vagamon at JMount Resorts. Our photo gallery captures the natural beauty of the region, from lush hills to tranquil lakes. Book your stay now and experience the enchanting beauty of Vagamon for yourself."
         />
         <link rel="icon" type="image/png" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#FFFFFF" />
         <meta name="theme-color" content="#FFFFFF" />
@@ -67,18 +124,36 @@ export default function Gallery() {
           name="keywords"
           content="resort, Vagamon, Kerala, hill resort, accommodations, tree house, restaurant, room service, spa, outdoor pool, trekking, paragliding, rock climbing, resort at vagamon, jmount vagamon, jmount vagamon resorts, resort offers, experience resort, most popular resort, resort experience, resort nature, Vagamon resorts, Kerala hill resorts ,Western Ghats resorts ,Tea plantation resorts ,Nature resorts ,Adventure resorts ,Luxury resorts ,Relaxation resorts ,Spa resorts ,Romantic getaways ,Family vacations ,Hill station vacations ,Hill station getaways ,Western Ghats tourism ,Kerala tourism ,Tea plantation tours ,Nature tours ,Adventure tours ,Luxury travel ,Relaxation vacations ,Spa vacations ,Romantic holidays ,Family holidays ,Honeymoon destinations"
         />
-        
+
         {/*<!-- Open Graph / Facebook -->*/}
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="JMount Gallery - A Picture is worth a Thousand Words, See the Beauty of Vagamon through our Gallery" />
-        <meta property="og:description" content="Explore the idyllic views of Vagamon at JMount Resorts. Our photo gallery captures the natural beauty of the region, from lush hills to tranquil lakes. Book your stay now and experience the enchanting beauty of Vagamon for yourself." />
-        <meta property="og:image" content="https://jmountvagamon.in/og-image.jpg" />
+        <meta
+          property="og:title"
+          content="JMount Gallery - A Picture is worth a Thousand Words, See the Beauty of Vagamon through our Gallery"
+        />
+        <meta
+          property="og:description"
+          content="Explore the idyllic views of Vagamon at JMount Resorts. Our photo gallery captures the natural beauty of the region, from lush hills to tranquil lakes. Book your stay now and experience the enchanting beauty of Vagamon for yourself."
+        />
+        <meta
+          property="og:image"
+          content="https://jmountvagamon.in/og-image.jpg"
+        />
 
         {/*<!-- Twitter -->*/}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="JMount Gallery - A Picture is worth a Thousand Words, See the Beauty of Vagamon through our Gallery" />
-        <meta property="twitter:description" content="Explore the idyllic views of Vagamon at JMount Resorts. Our photo gallery captures the natural beauty of the region, from lush hills to tranquil lakes. Book your stay now and experience the enchanting beauty of Vagamon for yourself." />
-        <meta property="twitter:image" content="https://jmountvagamon.in/og-image.jpg" />
+        <meta
+          property="twitter:title"
+          content="JMount Gallery - A Picture is worth a Thousand Words, See the Beauty of Vagamon through our Gallery"
+        />
+        <meta
+          property="twitter:description"
+          content="Explore the idyllic views of Vagamon at JMount Resorts. Our photo gallery captures the natural beauty of the region, from lush hills to tranquil lakes. Book your stay now and experience the enchanting beauty of Vagamon for yourself."
+        />
+        <meta
+          property="twitter:image"
+          content="https://jmountvagamon.in/og-image.jpg"
+        />
 
         <meta name="author" content="JMount Vagamon" />
       </Head>
@@ -87,44 +162,25 @@ export default function Gallery() {
           Gallery
         </Title>
         <Container my="md">
-          <SimpleGrid cols={3} spacing="xl" verticalSpacing="xl">
-            {images?.map((e) => (
-              <a key={e.url} href={e.url} target="_blank" rel="noreferrer">
-                <Box
-                  style={{ background: `url(${e.url})`, backgroundSize: "cover", height: 180, borderRadius: 5 }}
-                ></Box>
-              </a>
-            ))}
-          </SimpleGrid>
-          {/* {images?.map((e) => (
-            <SimpleGrid cols={4} breakpoints={[{ maxWidth: "xs", cols: 1 }]} key={e.url} mt={15}>
-              {getChild(e.url, BASE_HEIGHT)}
-              <Stack>
-                {getChild(e.url, getSubHeight(2, theme.spacing.md))}
-                {getChild(e.url, getSubHeight(2, theme.spacing.md))}
-              </Stack>
-              <Stack>
-                {getChild(e.url, getSubHeight(3, theme.spacing.md))}
-                {getChild(e.url, getSubHeight(3, theme.spacing.md))}
-                {getChild(e.url, getSubHeight(3, theme.spacing.md))}
-              </Stack>
-              {getChild(e.url, BASE_HEIGHT)}
-            </SimpleGrid>
-          ))}
-
-          <SimpleGrid cols={4} breakpoints={[{ maxWidth: "xs", cols: 1 }]} mt={20}>
-            {getChildSkeleton(BASE_HEIGHT)}
-            <Stack>
-              {getChildSkeleton(getSubHeight(2, theme.spacing.md))}
-              {getChildSkeleton(getSubHeight(2, theme.spacing.md))}
-            </Stack>
-            <Stack>
-              {getChildSkeleton(getSubHeight(3, theme.spacing.md))}
-              {getChildSkeleton(getSubHeight(3, theme.spacing.md))}
-              {getChildSkeleton(getSubHeight(3, theme.spacing.md))}
-            </Stack>
-            {getChildSkeleton(BASE_HEIGHT)}
-          </SimpleGrid> */}
+          <div>
+            <GridGallery
+              images={galleryImages}
+              onClick={handleClick}
+              enableImageSelection={false}
+              thumbnailStyle={{
+                objectFit: "contain",
+                width: "100%",
+                height: "100%",
+                borderRadius: 10,
+              }}
+            />
+            <Lightbox
+              slides={slideImages}
+              open={index >= 0}
+              index={index}
+              close={() => setIndex(-1)}
+            />
+          </div>
         </Container>
       </Container>
     </>
